027288c7a52923b84fbb1069d120f72e9cb7fcfc
cmachsocket <cmach_socket@outlook.com>
2025-11-16 23:31:21 +0800

    little optimize

--- cuda.cu
+++ cuda.cu
@@ -2,6 +2,8 @@
 #include "checkerboard.h"
 #define SELF_SCORE 1
 __constant__ int SCORES[6]{0, 1, 10, 100, 1000, 10000};
+__constant__ int cuda_step_x[MAX_DIRECT + 1]{0, 1, -1, 0, 1};
+__constant__ int cuda_step_y[MAX_DIRECT + 1]{0, 1, 1, 1, 0};
 __device__ int cuda_board[MAX_ROW][MAX_COL];
 __device__ int cuda_board_access[MAX_ROW][MAX_COL];
 __managed__ int cuda_ans[MAX_ROW][MAX_COL];
@@ -39,16 +41,16 @@ __device__ __inline__ bool cuda_is_inside(int x, int y) {
     return ((x >= 0 and x < MAX_ROW) and (y >= 0 and y < MAX_COL));
 }
 
-__device__ __inline__ pair empty_extend(int direct, int _player, int x, int y, const int cuda_step_x[], const int cuda_step_y[]) {
+__device__ __inline__ pair empty_extend(int direct, int _player, int x, int y,int dir) {
     int count = 0, extend_count = 0;
     for (; cuda_is_inside(x, y) and cuda_board[x][y] == _player;
-           count++, x += cuda_step_x[direct], y += cuda_step_y[direct]) {
+           count++, x += dir*cuda_step_x[direct], y += dir*cuda_step_y[direct]) {
 
     }
     if (cuda_is_inside(x, y) and cuda_board[x][y] == EMPTY_POS) { //可以继续从空点扩展,前提是扩展后有对应棋子
-        for (x += cuda_step_x[direct], y += cuda_step_y[direct];
+        for (x += dir*cuda_step_x[direct], y += dir*cuda_step_y[direct];
             cuda_is_inside(x,y) and cuda_board[x][y] == _player;
-            extend_count++, x += cuda_step_x[direct], y += cuda_step_y[direct]) {
+            extend_count++, x += dir*cuda_step_x[direct], y += dir*cuda_step_y[direct]) {
         }
         if (count or extend_count)extend_count++;
     }
@@ -56,26 +58,21 @@ __device__ __inline__ pair empty_extend(int direct, int _player, int x, int y, c
 }
 
 __device__ __inline__ int clac_extend(int direct, int x, int y, int ply, int cuda_step_x[], int cuda_step_y[]) {
-    auto [count_1,empty_extend_tot_1] = empty_extend(direct, ply, x + cuda_step_x[direct], y + cuda_step_y[direct], cuda_step_x, cuda_step_y);
+    auto [count_1,empty_extend_tot_1] = empty_extend(direct, ply, x + cuda_step_x[direct], y + cuda_step_y[direct], 1);
     cuda_step_x[direct] = -cuda_step_x[direct], cuda_step_y[direct] = -cuda_step_y[direct]; //改变方向
 
-    auto [count_2,empty_extend_tot_2] = empty_extend(direct, ply, x + cuda_step_x[direct], y + cuda_step_y[direct], cuda_step_x, cuda_step_y);
+    auto [count_2,empty_extend_tot_2] = empty_extend(direct, ply, x - cuda_step_x[direct], y - cuda_step_y[direct],-1);
     return count_1 + count_2 + SELF_SCORE + max(empty_extend_tot_1 , empty_extend_tot_2);//决定扩展方向
 
 }
 
 __global__ void clac_single_pos(int ply) {
-    while (true) {
-
-    }
     int x = static_cast<int>(blockIdx.x);
     int y = static_cast<int>(threadIdx.x);
     cuda_ans[x][y] = 0;
     if (y >= MAX_COL or !cuda_board[x][y]==EMPTY_POS  or !cuda_board_access[x][y]) {
         return;
     }
-    int cuda_step_x[MAX_DIRECT + 1]{0, 1, -1, 0, 1};
-    int cuda_step_y[MAX_DIRECT + 1]{0, 1, 1, 1, 0};
     int tri_count = 0, _ans = 0;
     for (int i = 1; i <= MAX_DIRECT; i++) {
         int tmp = clac_extend(i, x, y, ply, cuda_step_x, cuda_step_y);
@@ -84,16 +81,15 @@ __global__ void clac_single_pos(int ply) {
         if (tri_count > 1)_ans += SCORES[MAX_SCORE];
         else _ans += SCORES[tmp];
     }
-    cuda_ans[x][y] += _ans;
 
     //为提升性能重复利用
-    tri_count = 0, _ans = 0;
+    tri_count = 0;
     for (int i = 1; i <= MAX_DIRECT; i++) {
         int tmp = clac_extend(i, x, y, -ply, cuda_step_x, cuda_step_y);
         if (tmp>5)tmp=5;
         if (tmp >= 4)tri_count++;
-        if (tri_count > 1)_ans += SCORES[MAX_SCORE];
-        else _ans += SCORES[tmp];
+        if (tri_count > 1)_ans -= SCORES[MAX_SCORE];
+        else _ans -= SCORES[tmp];
 
     }
     cuda_ans[x][y] -= _ans;
--- libcuda.cuh
+++ libcuda.cuh
@@ -9,8 +9,8 @@ struct pair {
 };
 
 __device__ __inline__ bool cuda_is_inside(int x, int y);
-__device__ __inline__ pair empty_extend(int direct,int _player, int x, int y,const int cuda_step_x[],const int cuda_step_y[]);
-__device__ __inline__ int clac_extend(int direct, int x, int y , int ply,int cuda_step_x[],int cuda_step_y[]);
+__device__ __inline__ pair empty_extend(int direct,int _player, int x, int y,int dir);
+__device__ __inline__ int clac_extend(int direct, int x, int y , int ply);
 __global__ void clac_single_pos(int ply);
 
 extern "C"
